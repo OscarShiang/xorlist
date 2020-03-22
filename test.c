3bd75231071d0c03ed326ab240d3d84d161558ed
Oscar Shiang <oscar0225@livemail.tw>
2020-03-22 16:27:39 +0800

    Initialize the list pointers
    Assign the list pointers in test.c
    to prevent from occurring segmentation fault

--- test.c
+++ test.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[]) {
   srand(time(NULL));
   int n = atoi(argv[1]);
 
-  list *a, *b;
+  list *a = NULL, *b = NULL;
 
   // insert nodes with the given n
   for (int i = 0; i < n; i++) {
@@ -19,9 +19,15 @@ int main(int argc, char *argv[]) {
     insert_node(&b, tmp);
   }
 
+  printf("the original list:\n");
   print_list(a);
-  a = sort_list(a);
+  
+  printf("the sorted list:\n");
+  a = sort(a);
   print_list(a);
+
+  b = sort_list(b);
+  print_list(b);
 
   delete_list(a);
   delete_list(b);
